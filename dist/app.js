6f0b5f22a149e6401b66cfad713a1049a9bc67b9
ametistabot <aestheticpag1@gmail.com>
2025-08-26 10:01:38 -0400

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -3,6 +3,13 @@ moment.tz.setDefault('America/Sao_Paulo');
 import { botUpdater } from './helpers/bot.updater.helper.js';
 import connect from './socket.js';
 import ffmpeg from "fluent-ffmpeg";
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("Bot rodando ✅"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor HTTP rodando na porta ${PORT}`);
+});
 import { buildText, getCurrentBotVersion } from "./utils/general.util.js";
 import botTexts from "./helpers/bot.texts.helper.js";
 import('@ffmpeg-installer/ffmpeg').then((ffmpegInstaller) => {
